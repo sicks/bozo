1ee432c1bf9ba93b7615feb1f6cd711dcb00912a
sicks <tcopeland@dg3n.net>
2015-11-04 02:47:43 +0000

    refactored user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,14 @@ class User < ActiveRecord::Base
   devise :registerable, :rememberable, :trackable
   devise :omniauthable, omniauth_providers: [:google_oauth2, :steam]
 
+  validates :username, presence: true, uniqueness: true
+
   has_many :auths, inverse_of: :user, dependent: :destroy
   accepts_nested_attributes_for :auths
   validates_associated :auths
 
-  def self.from_omniauth( auth )
-    user = joins(:auths).where("auths.provider = ? AND auths.uid = ?", auth.provider, auth.uid).first
+  def self.from_omniauth( auth_hash )
+    user = joins(:auths).where("auths.provider = ? AND auths.uid = ?", auth_hash.provider, auth_hash.uid).first
     user.nil? ? false : user
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,38 +1,50 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+RSpec.describe 'app/models/user.rb', type: :model do
   let!(:user) { create(:user) }
   let(:auth_hash) { OmniAuth::AuthHash.new( attributes_for :steam_auth ) }
 
-  it 'has a valid factory' do
-    expect( user.valid? ).to eq true
-  end
-
-  it 'is invalid with invalid auths' do
-    user.auths.first.provider = nil
-    expect( user.valid? ).to eq false
-  end
-
-  describe ".from_omniauth" do
-    let(:new_user) { User.from_omniauth( auth_hash ) }
-
-    context "when this user's already registered" do
-      before(:example) do
-        user
-      end
-
-      it "returns the registered user" do
-        expect( new_user ).to eq user
+  describe User,'validates' do
+    %w[username].each do |attribute|
+      specify "presence of #{attribute}" do
+        setter = attribute + "="
+        user.send setter.to_sym, nil
+        expect( user.valid? ).to eq false
       end
     end
 
-    context "when this user doesn't exist yet" do
-      before(:example) do
-        User.destroy_all
+    specify "uniqueness of username" do
+      expect( user.dup.valid? ).to eq false
+    end
+
+    specify 'validity of auths' do
+      user.auths.first.provider = nil
+      expect( user.valid? ).to eq false
+    end
+  end
+
+  describe User, "class methods" do
+    describe ".from_omniauth( auth_hash )" do
+      let(:new_user) { User.from_omniauth( auth_hash ) }
+
+      context "when this user's already registered" do
+        before(:example) do
+          user
+        end
+
+        it "returns the registered user" do
+          expect( new_user ).to eq user
+        end
       end
 
-      it "returns false" do
-        expect( new_user ).to eq false
+      context "when this user doesn't exist yet" do
+        before(:example) do
+          User.destroy_all
+        end
+
+        it "returns false" do
+          expect( new_user ).to eq false
+        end
       end
     end
   end
